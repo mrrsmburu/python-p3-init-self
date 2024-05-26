1aac7c79cefc2e30650904aa53bfc4aed483a5b6
mrrsmburu <mrrsmburu@gmail.com>
2024-05-26 12:56:22 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
 class Dog:
-    
-    def __init__(self, name, breed="Mutt"):
+    def __init__(self, name, breed ="Mutt"):
         self.name = name
         self.breed = breed
-    pass
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
 class Person:
-    
     def __init__(self, name):
-        self.name = name
-    pass
+        self.name=name
